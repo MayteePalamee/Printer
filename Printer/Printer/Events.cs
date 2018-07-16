d69f9c402efba1d7dcefdd5d50178bf680bcb51c
MayteePalamee <m.palami@hotmail.com>
2018-07-16 14:19:32 +0700

    Initial commit

---
+++ Printer/Printer/Events.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer
+{
+    public class Events : EventArgs
+    {
+        private readonly String _message;
+        public Events(String message)
+        {
+            _message = message;
+        }
+        public String Message
+        {
+            get { return _message; }
+        }
+    }
+}
---
+++ Printer/Printer/Printers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer
+{
+    public class Printers
+    {
+        /**
+      * event delegate.
+      * Declare the event using EventHandler<T>
+      **/
+
+        public event EventHandler<Events> RaiseEvents;
+
+        /**
+         * delegate method handle raise event
+         **/
+        protected virtual void OnRaiseEvent(Events e)
+        {
+            RaiseEvents?.Invoke(this, e);
+        }
+
+        Status status;
+        public Status Connect()
+        {
+            status = new Status();
+            try
+            {
+
+            }catch(Exception e)
+            {
+
+            }
+            return status;
+        }
+        public Boolean Printing()
+        {
+            try
+            {
+
+            }catch(Exception e)
+            {
+
+            }
+            return true;
+        }
+    }
+}
